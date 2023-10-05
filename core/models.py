79f5001f08eb3044774470b68304f8e9aea19ed3
Dmitriy Masitsev <108647851+Gysborn@users.noreply.github.com>
2023-10-05 21:30:36 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class User(AbstractUser):
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = []# При создании  юзера не запрашивает email
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
