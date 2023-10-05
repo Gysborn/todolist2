b3f07498421b1414ecf46567e17f364b20a84fea
Dmitriy Masitsev <108647851+Gysborn@users.noreply.github.com>
2023-10-05 21:32:53 +0300

    Update settings.py

--- todolist2/settings.py
+++ todolist2/settings.py
@@ -114,7 +114,7 @@ AUTH_USER_MODEL = 'core.User'
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
